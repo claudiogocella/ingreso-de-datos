22b8d64e5c69db25c325453de072314d73005d97
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-29 14:40:47 -0300

    agregué boton enviar y botón rotulo agregué botones y funciones a ejecutar. Y lo probé por consola.

--- src/datos.ts
+++ src/datos.ts
@@ -1,1 +1,7 @@
 let btnEnv = document.getElementById("btnEnviar");
+let dato = document.forms[0].dato;
+let rotulo = document.getElementById("rotulo");
+
+btnEnv?.addEventListener("click", () => {
+  console.log("el dato ingresado es ", dato.value);
+});
