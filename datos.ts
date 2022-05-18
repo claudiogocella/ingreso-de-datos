c1b49f6c93a0d89619b1e71b5e7cc5d822c0c0d4
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-18 19:23:51 -0300

    modifiqué texto agregué texto luego de volver a mirar la clase de Karen

--- datos.ts
+++ datos.ts
@@ -3,8 +3,6 @@ let dato = document.forms[0].dato;
 let rotulo = document.getElementById("rotulo");
 
 btnEnv?.addEventListener("click", () => {
-  //TU CÓDIGO VA AQUÍ, POR EJEMPLO:
+//TU CÓFIGO VA AQUÍ, POR EJEMPLO:
   console.log("el dato ingresado es ", dato.value);
 });
-
-rotulo?.innerHTML = "Ingresar dato...";
